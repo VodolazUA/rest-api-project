b02464a5a0e37b3ee1d97bc93e3d0cf07379634f
Sashs E <svitae@gmail.com>
2023-11-13 10:32:40 -0800

    ddd ssss 112312312312

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 
-# tttt 111
+# tttt 111 222
 
 from db import db
 from blocklist import BLOCKLIST
